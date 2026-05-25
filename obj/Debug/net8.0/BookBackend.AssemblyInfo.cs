68f03060781dfbdfeee11fd9f46b60cf63d9512d
Luis Acosta <luis2011-11@hotmail.com>
2026-05-25 13:37:45 -0500

    Correcion HomoVictor:Version--vxd

--- obj/Debug/net8.0/BookBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/BookBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fe47056120d541737dd46df2f7f608998c7b897")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
